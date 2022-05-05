abba8b08b217c8f3caa4963743a8ac0aacf9be45
F-a-y-t-h <14098907+F-a-y-t-h@users.noreply.github.com>
2022-05-05 06:32:22 +0100

    Create cobblegen.lua

---
+++ cobblegen.lua
@@ -0,0 +1,9 @@
+local component = require("component")
+local computer = require("computer")
+local robot = require("robot")
+local shell = require("shell")
+local sides = require("sides")
+
+while true do
+ r.swing(sides.forward)
+end
